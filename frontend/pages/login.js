8a563bca0935c4cbf3682b5493721f885c19ec90
Enes0668 <maxigaming949@gmail.com>
2025-08-15 05:42:29 +0300

    Trying one

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -7,10 +7,11 @@ export default function Login() {
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const router = useRouter();
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('${API_URL}/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
