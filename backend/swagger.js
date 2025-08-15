a5abc93d8f2f455df695b5da79906687a84a9798
Enes0668 <maxigaming949@gmail.com>
2025-08-16 02:02:50 +0300

    Swagger test 4

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,27 +1,30 @@
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const path = require("path");
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Auth API",
-      version: "1.0.0",
-      description: "Kullanıcı kimlik doğrulama ve OTP işlemleri için API",
-    },
-    servers: [
-      {
-        url: "https://ecommerce-app-1-bpok.onrender.com", // auth router base path
+const setupSwagger = (app) => {
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Auth API",
+        version: "1.0.0",
+        description: "Kullanıcı kimlik doğrulama ve OTP işlemleri için API"
       },
-    ],
-  },
-  apis: ["./routes/auth.js", "./routes/cart.js", "./routes/category.js", "./routes/order.js", "./routes/payment.js", "./routes/productRoutes.js", "./routes/protectedRoute.js", "./routes/userRoutes.js"], // swagger açıklamalarını auth.js içinde de kullanabiliriz
+      servers: [
+        { url: "https://ecommerce-app-1-bpok.onrender.com" } // render URL’in
+      ]
+    },
+    apis: ["./routes/auth.js", "./routes/cart.js", "./routes/category.js", "./routes/order.js", "./routes/payment.js", "./routes/productRoutes.js", "./routes/protectedRoute.js", "./routes/userRoutes.js"], // swagger açıklamalarını auth.js içinde de kullanabiliriz // Swagger yorumlarının olduğu route dosyaları
+  };
+
+  const swaggerSpec = swaggerJsDoc(options);
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
-const swaggerSpec = swaggerJsDoc(options);
-
-function setupSwagger(app) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-}
-
 module.exports = setupSwagger;
+
+
+
+
+  
