5b0b083734f5b192557df3fd9fbe4ae610797c90
Enes0668 <maxigaming949@gmail.com>
2025-08-15 04:49:33 +0300

    In order to deployment process,I'm looking sensitive code

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
+const User = require('../models/User');
 
 // Tüm kullanıcıları getir (Admin için)
 router.get('/', async (req, res) => {
